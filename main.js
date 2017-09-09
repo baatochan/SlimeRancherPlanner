f3ba56d9a3843fbe7975b5f2e25f7c0c4ee4c10a
Bartosz Rodziewicz <bartoszka1996@gmail.com>
2017-09-09 16:23:49 +0200

    Alignment of maximazed plots added

--- main.js
+++ main.js
@@ -4,61 +4,61 @@
 
 var plots = [
 	{
-		'id': 1, 'leftPosition': 341, 'topPosition': 614
+		'id': 1, 'leftPosition': 341, 'topPosition': 614, 'align': 'bottom-right'
 	}, {
-		'id': 2, 'leftPosition': 341, 'topPosition': 655
+		'id': 2, 'leftPosition': 341, 'topPosition': 655, 'align': 'top-right'
 	}, {
-		'id': 3, 'leftPosition': 381, 'topPosition': 614
+		'id': 3, 'leftPosition': 381, 'topPosition': 614, 'align': 'bottom-left'
 	}, {
-		'id': 4, 'leftPosition': 381, 'topPosition': 655
+		'id': 4, 'leftPosition': 381, 'topPosition': 655, 'align': 'top-left'
 	}, {
-		'id': 5, 'leftPosition': 441, 'topPosition': 365
+		'id': 5, 'leftPosition': 441, 'topPosition': 365, 'align': 'top-right'
 	}, {
-		'id': 6, 'leftPosition': 489, 'topPosition': 268
+		'id': 6, 'leftPosition': 489, 'topPosition': 268, 'align': 'bottom-right'
 	}, {
-		'id': 7, 'leftPosition': 552, 'topPosition': 270
+		'id': 7, 'leftPosition': 552, 'topPosition': 270, 'align': 'bottom-left'
 	}, {
-		'id': 8, 'leftPosition': 561, 'topPosition': 363
+		'id': 8, 'leftPosition': 561, 'topPosition': 363, 'align': 'top-left'
 	}, {
-		'id': 9, 'leftPosition': 776, 'topPosition': 192
+		'id': 9, 'leftPosition': 776, 'topPosition': 192, 'align': 'bottom-right'
 	}, {
-		'id': 10, 'leftPosition': 775, 'topPosition': 232
+		'id': 10, 'leftPosition': 775, 'topPosition': 232, 'align': 'top-right'
 	}, {
-		'id': 11, 'leftPosition': 893, 'topPosition': 191
+		'id': 11, 'leftPosition': 893, 'topPosition': 191, 'align': 'bottom-right'
 	}, {
-		'id': 12, 'leftPosition': 893, 'topPosition': 232
+		'id': 12, 'leftPosition': 893, 'topPosition': 232, 'align': 'top-right'
 	}, {
-		'id': 13, 'leftPosition': 893, 'topPosition': 318
+		'id': 13, 'leftPosition': 893, 'topPosition': 318, 'align': 'top-right'
 	}, {
-		'id': 14, 'leftPosition': 934, 'topPosition': 191
+		'id': 14, 'leftPosition': 934, 'topPosition': 191, 'align': 'bottom-left'
 	}, {
-		'id': 15, 'leftPosition': 934, 'topPosition': 232
+		'id': 15, 'leftPosition': 934, 'topPosition': 232, 'align': 'top-left'
 	}, {
-		'id': 16, 'leftPosition': 934, 'topPosition': 318
+		'id': 16, 'leftPosition': 934, 'topPosition': 318, 'align': 'top-left'
 	}, {
-		'id': 17, 'leftPosition': 1069, 'topPosition': 595
+		'id': 17, 'leftPosition': 1069, 'topPosition': 595, 'align': 'bottom-right'
 	}, {
-		'id': 18, 'leftPosition': 1069, 'topPosition': 648
+		'id': 18, 'leftPosition': 1069, 'topPosition': 648, 'align': 'top-right'
 	}, {
-		'id': 19, 'leftPosition': 1108, 'topPosition': 595
+		'id': 19, 'leftPosition': 1108, 'topPosition': 595, 'align': 'bottom-left'
 	}, {
-		'id': 20, 'leftPosition': 1108, 'topPosition': 648
+		'id': 20, 'leftPosition': 1108, 'topPosition': 648, 'align': 'top-left'
 	}, {
-		'id': 21, 'leftPosition': 1150, 'topPosition': 708
+		'id': 21, 'leftPosition': 1150, 'topPosition': 708, 'align': 'top-left'
 	}, {
-		'id': 22, 'leftPosition': 1152, 'topPosition': 233
+		'id': 22, 'leftPosition': 1152, 'topPosition': 233, 'align': 'bottom-right'
 	}, {
-		'id': 23, 'leftPosition': 1167, 'topPosition': 305
+		'id': 23, 'leftPosition': 1167, 'topPosition': 305, 'align': 'top-right'
 	}, {
-		'id': 24, 'leftPosition': 1218, 'topPosition': 222
+		'id': 24, 'leftPosition': 1218, 'topPosition': 222, 'align': 'bottom-right'
 	}, {
-		'id': 25, 'leftPosition': 1262, 'topPosition': 248
+		'id': 25, 'leftPosition': 1262, 'topPosition': 248, 'align': 'top-left'
 	}, {
-		'id': 26, 'leftPosition': 1258, 'topPosition': 325
+		'id': 26, 'leftPosition': 1258, 'topPosition': 325, 'align': 'top-left'
 	}
 ];
 
-var waterfallPond = {'id': 27, 'leftPosition': 374, 'topPosition': 570};
+var waterfallPond = {'id': 27, 'leftPosition': 374, 'topPosition': 570, 'align': 'bottom-right'};
 
 $(document).ready(function (e) {
 	showPlots();
